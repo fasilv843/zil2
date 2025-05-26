262f8d4418dcdc3e66c87b906a5e8736ee6bf96a
Fasil <fasilvaliyattil843@gmail.com>
2025-05-26 16:13:32 +0530

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'zilmoney2';
+  cities = signal(['Mumbai', 'Kolkata', 'Delhi']);
+
+  form = new FormGroup({
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5)
+      ]
+    }),
+    lastName: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.maxLength(10)
+      ]
+    }),
+    age: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.min(17),
+        Validators.max(20),
+      ]
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.pattern(``) // add email regex
+      ]
+    }),
+    phone: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        // Validators.pattern('/[0-9]${10}/')
+      ]
+    }),
+    city: new FormControl('', {
+      nonNullable: true,
+      validators: [
+
+      ]
+    }),
+    gender: new FormControl('', {
+      nonNullable: true,
+      validators: [
+
+      ]
+    }),
+    militaryStatus: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [
+
+      ]
+    }),
+  })
+
+  onSubmit() {
+
+  }
 }
